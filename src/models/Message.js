31d2823255ae173c7044f7fd90fa67e6db2543d6
ChanwooBrianKim <151323407+ChanwooBrianKim@users.noreply.github.com>
2024-08-05 00:45:18 +1000

    DB for message log

---
+++ src/models/Message.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    room: { type: String, required: true },
+    nickname: { type: String, required: true },
+    message: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+})
