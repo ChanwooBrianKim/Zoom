6976b52ff55f315735909744a69522ae7612b726
ChanwooBrianKim <brian980729@gmail.com>
2024-08-05 15:29:10 +1000

    Test

--- src/test.js
+++ src/test.js
@@ -1,51 +1,54 @@
-import http from 'http';
-import express from 'express';
-import { Server } from 'socket.io';
-import { instrument } from '@socket.io/admin-ui';
-import bodyParser from 'body-parser';
-import path from 'path';
-import dotenv from 'dotenv';
-import './db.js'; // Ensure db.js is imported to establish the database connection
+import Message from './models/Message.js';
 
-import authRoutes from './routes/auth.js';
-import uploadRoutes from './routes/upload.js';
-import { authenticateSocket } from './middlewares/auth.js';
-import { publicRooms, countRoom, handleSocketConnection } from './socket.js';
+export function publicRooms(wsServer) {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
 
-dotenv.config();
-const app = express();
-const SECRET_KEY = process.env.SECRET_KEY;
+export function countRoom(roomName, wsServer) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('view engine', 'pug');
-app.set('views', path.join(path.resolve(), 'views'));
-app.use('/public', express.static(path.join(path.resolve(), 'public')));
-app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
-
-app.get('/', (_, res) => res.render('home'));
-app.get('/login', (_, res) => res.render('login'));
-app.get('/register', (_, res) => res.render('register'));
-app.get('/*', (_, res) => res.redirect('/'));
-
-app.use('/auth', authRoutes);
-app.use('/upload', authenticateSocket, uploadRoutes);
-
-const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ['https://admin.socket.io'],
-    credentials: true,
-  },
-});
-
-instrument(wsServer, {
-  auth: false,
-});
-
-wsServer.use(authenticateSocket);
-wsServer.on('connection', (socket) => handleSocketConnection(socket, wsServer));
-
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);
+export function handleSocketConnection(socket, wsServer) {
+  socket['nickname'] = socket.user.username;
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName, wsServer));
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit('bye', socket.nickname, countRoom(room, wsServer) - 1)
+    );
+  });
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms(wsServer));
+  });
+  socket.on('new_message', async (msg, room, done) => {
+    const message = new Message({
+      room,
+      nickname: socket.nickname,
+      message: msg,
+    });
+    await message.save();
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  });
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
+  socket.on('file_shared', (filePath) => {
+    wsServer.emit('file_shared', filePath);
+  });
+}
