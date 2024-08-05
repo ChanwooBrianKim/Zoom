a7c7ae57c63a2aae826a2a613670c8e0321e97b4
ChanwooBrianKim <brian980729@gmail.com>
2024-08-05 13:21:38 +1000

    comments for routes folder updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs'; // library for hashing passwords
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 const router = express.Router();
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = process.env.SECRET_KEY; // Secured in .env file
 
+// Registration route
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10); // Hashes the password using bcrypt with a salt factor of 10
   try {
-    const user = new User({ username, password: hashedPassword });
+    const user = new User({ username, password: hashedPassword }); // Create new user
     await user.save();
     res.status(201).send({ message: 'User registered successfully' });
   } catch (error) {
@@ -18,12 +19,14 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// Login route
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
+  // if the users exists & hashed password matches & sets an expiration of 1h
   if (user && await bcrypt.compare(password, user.password)) {
     const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
-    res.send({ token });
+    res.send({ token }); // sends token in the response
   } else {
     res.status(401).send({ error: 'Invalid username or password' });
   }
--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,23 +1,26 @@
 import express from 'express';
-import multer from 'multer';
+import multer from 'multer'; // Middleware for handling multipart/form-data for file uploads
 import path from 'path';
 
 const router = express.Router();
 
+// Configuration for multer to specify where and how files should be stored
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'uploads/'); // Specifies the directory
     },
     filename: (req, file, cb) => {
+        // timestamp combined with the original file extension
         cb(null, Date.now() + path.extname(file.originalname));
     },
 });
 
+// Creates an instance of multer
 const upload = multer({ storage });
 
 router.post('/', upload.single('file'), (req, res) => {
     if (req.file) {
-        res.send({ filePath: `/uploads/${req/file.filename}`});
+        res.send({ filePath: `/uploads/${req.file.filename}`});
     } else {
         res.status(500).send({ error: 'File uploadd failed' });
     }
