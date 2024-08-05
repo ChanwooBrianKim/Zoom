c0aae8e6738b5eb6d3dcc58a3a3c1bcde81c3540
ChanwooBrianKim <brian980729@gmail.com>
2024-08-05 15:28:11 +1000

    db comment updated

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; // A module to load environment variables from a '.env' file into 'process.env'
 
 dotenv.config(); // Load environment variables from .env file
 
 console.log('MONGODB_URI in db.js:', process.env.MONGODB_URI); // Debug statement
 
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = process.env.MONGODB_URI; // debug statement logs (loaded correctly?)
 
 if (!mongoURI) {
   console.error('MONGODB_URI is not defined in the .env file');
@@ -16,10 +16,12 @@ mongoose.connect(mongoURI).catch(error => console.error('Initial connection erro
 
 const db = mongoose.connection;
 
+// error event listener to log any connection errors
 db.on('error', (error) => {
   console.error('Connection error:', error);
 });
 
+//  event listener to log a success message when the connection is successfully opened
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
