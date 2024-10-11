ae7041814fc16e2b3fc82fb6da2dd991e8c74298
ChanwooBrianKim <brian980729@gmail.com>
2024-10-11 12:14:16 +1000

    Comment

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,14 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; // Importing mongoose to define and use the schema and model
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; // Creating a shortcut for mongoose.Schema for easier reference
 
+// Defining the schema for a message in the chat application
 const messageSchema = new Schema({
-    room: { type: String, required: true },
-    nickname: { type: String, required: true },
-    message: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now },
-})
+    room: { type: String, required: true }, // The name of the room where the message was sent, required field
+    nickname: { type: String, required: true }, // The nickname of the user who sent the message, required field
+    message: { type: String, required: true }, // The content of the message, required field
+    timestamp: { type: Date, default: Date.now }, // The timestamp when the message was sent, default value is the current date and time
+});
 
+// Creating the Message model from the schema, which will allow interaction with the 'messages' collection in MongoDB
 const Message = mongoose.model('Message', messageSchema);
 
+// Exporting the Message model so it can be used in other parts of the application
 export default Message;
