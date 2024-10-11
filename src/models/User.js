9ddae811f990cf13fe30aaee2470e1ef9469c615
ChanwooBrianKim <brian980729@gmail.com>
2024-10-11 12:14:21 +1000

    Commented

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; // Importing mongoose to define and use schema and model
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema; // Creating a shortcut for mongoose.Schema for easier reference
 
+// Defining the schema for a user in the application
 const userSchema = new Schema({
-    username: { type: String, unique: true, requried: true},
-    password: { type: String, reqeuired: true},
+    username: { type: String, unique: true, required: true }, // The username, it must be unique (no duplicates allowed) and is required
+    password: { type: String, required: true }, // The hashed password, which is also a required field
 });
 
+// Creating the User model from the schema, which will allow interaction with the 'users' collection in MongoDB
 const User = mongoose.model('User', userSchema);
 
+// Exporting the User model so it can be used in other parts of the application
 export default User;
